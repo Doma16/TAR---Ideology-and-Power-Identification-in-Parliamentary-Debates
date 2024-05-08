1dea61049cb86d9226aea6467fe3d45846a7a020
Doma16 <domasaric@gmail.com>
2024-05-09 01:48:58 +0200

    few changes

--- data_loader.py
+++ data_loader.py
@@ -5,6 +5,8 @@ from podium.datasets import TabularDataset
 from podium.vectorizers import GloVe
 import numpy as np
 
+import time
+
 class DataLoader:
     def __init__(self, file_path, batch_size, shuffle=True):
         
@@ -20,19 +22,17 @@ class DataLoader:
 
         self.build_vocab()
 
-
-
         if self.shuffle:
             random.shuffle(self.indices)
 
     def build_vocab(self):
         fixed_length = 1000
 
-        vocab = Vocab(min_freq=2)
+        self.vocab = Vocab(min_freq=2)
         S = Field(name='text',
-                    numericalizer=vocab,
-                    fixed_length=fixed_length,
-                    pretokenize_hooks=str.lower)
+                  numericalizer=self.vocab,
+                  fixed_length=fixed_length,
+                  pretokenize_hooks=str.lower)
         L = LabelField('label')
         fields = {
                 'text': S,
@@ -42,13 +42,14 @@ class DataLoader:
 
         train = TabularDataset.from_pandas(pd.read_csv(self.file_path, sep='\t'), fields)
         train.finalize_fields()
+        self.train_batch = train.batch(add_padding=True)
 
         self.train = train
         self.indices = list(range(len(train)))
         self.num_batches = int(np.ceil(len(train) / self.batch_size))
 
         glove = GloVe()
-        self.embeddings = glove.load_vocab(vocab)
+        self.embeddings = glove.load_vocab(self.vocab)
 
 
     def __iter__(self):
@@ -57,21 +58,13 @@ class DataLoader:
     def __next__(self):
         if self.current_index >= len(self.indices):
             raise StopIteration
-        
         batch_indices = self.indices[self.current_index:self.current_index + self.batch_size]
-        batch_data = [self.dataset.iloc[i] for i in batch_indices]
-
-
-        batch_df = pd.DataFrame(batch_data)
-        batch_tabular = TabularDataset.from_pandas(batch_df, self.fields)
-        train_batch = batch_tabular.batch(add_padding=True)
-       
-        batch_embeddings = self.embeddings[train_batch.text]
-        batch_labels = train_batch.label
+    
+        batch_data = [self.train_batch.text[i] for i in batch_indices]
+        batch_label = [self.train_batch.label[i] for i in batch_indices]
+    
+        batch_embeddings = self.embeddings[batch_data]
+        batch_labels = self.train_batch.label[batch_label]
 
         self.current_index += self.batch_size
-
         return batch_embeddings, batch_labels
-    
-
-
--- train_example.py
+++ train_example.py
@@ -23,15 +23,18 @@ def train_model(data):
         labels_1d = labels.reshape(-1)
         model.partial_fit(embeddings_2d, labels_1d, classes=np.unique(labels_1d))
 
-    
     return model
 
 
 if __name__ == '__main__':
     from data_loader import DataLoader
+    import time 
+    start = time.time()
     data = DataLoader(file_path="data/orientation_lemmatized_data.tsv", batch_size=32)
     trained_model= train_model(data)
     
     data2 = DataLoader(file_path="data/orientation_lemmatized_data.tsv", batch_size=32)
     f1 = calculate_f1(trained_model, data2)
     print(f1)
+    end = time.time()
+    print(f'Time: {(end-start):.3f}s')
